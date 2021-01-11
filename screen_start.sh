cc102a4c74aa8067b087490f56ba1edfbdd2f1cc
Spencer-Warren <75763632+Spencer-Warren@users.noreply.github.com>
2021-01-11 16:17:44 -0500

    Added random variable to shell script

--- screen_start.sh
+++ screen_start.sh
@@ -1,2 +1,4 @@
-screen -dmS server_screen sh -c "python /home/ec2-user/start.py"
-screen -S server_screen -r
+#!/bin/sh
+n=$RANDOM
+screen -dmS server_screen$n sh -c "python /home/ec2-user/start.py"
+screen -S server_screen$n -r
