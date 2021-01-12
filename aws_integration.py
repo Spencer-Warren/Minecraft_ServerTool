21a34ed93517013f869c67cf50dd5f91905f7cda
Spencer-Warren <75763632+Spencer-Warren@users.noreply.github.com>
2021-01-12 14:45:42 -0500

    added vpc request from user

--- aws_integration.py
+++ aws_integration.py
@@ -1,5 +1,6 @@
 import boto3
-from ssh_opener import option_parser
+from ssh_opener import option_parse
+
 def menu(options,pre=""):
     """
     Standardized menu function
@@ -33,25 +34,29 @@ def menu(options,pre=""):
 def write_setting(key, value):
     """
     Writes given option to options.txt file
-    takes
+    Args:
+        Key for option ex. VPC_id
+        Value to write to setting
     """
-    options = option_parser()
-    file = open("options.txt")
-    lines = file.readlines()
-    options = {}
-    for line in lines:
-        line = line.split("=")
-        options[line[0]] = line[1].rstrip()
+    options = option_parse()
+    options[key] = value
+    lines = []
+    for key in options:
+        lines.append(key+"="+options[key]+"\n")
+    file = open("options.txt","w")
+    file.writelines(lines)
     file.close()
     return options
 
 def main():
     ec2 = boto3.resource("ec2")
-    if option_parser()["VPC_id"] == None:
+    if option_parse()["VPC_id"] == "":
         vpc=input("Please enter your vpc id:\n")
-        vpc = ec2.Vpc("vpc")
+        write_setting("VPC_id", vpc)
+        vpc = ec2.Vpc(vpc)
     else:
-        vpc = ec2.Vpc(option_parser()["VPC_id"])
+        vpc = ec2.Vpc(option_parse()["VPC_id"])
+        print("vpc accepted...")
 
 if __name__ == "__main__":
     main()
