a978573fa66c6dfae907c1e286f17ac7c5c578e1
Spencer-Warren <75763632+Spencer-Warren@users.noreply.github.com>
2021-01-14 19:37:41 -0500

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,3 +1,18 @@
-python -m pip install awscli
-aws configure
-del "%~f0"
+@echo off
+python --version
+if errorlevel 1 goto :errorNoPython
+pip install boto3 --user
+pip install boto --user
+goto :delete
+:errorNoPython
+echo;
+echo Python is not installed on your system.
+echo or your PATH variable is not set correctly.
+echo Now opening the download URL.
+start "" "https://www.python.org/downloads/windows/"
+timeout 20
+goto :EOF
+:delete
+echo Packages installed!
+echo You can delete this file if you want
+timeout 15 
