8d533bcef9519ad93fa452e0af518f5e2d166ec2
Spencer-Warren <75763632+Spencer-Warren@users.noreply.github.com>
2021-01-14 20:33:16 -0500

    merge batch files

--- start.bat
+++ start.bat
@@ -1,3 +1,28 @@
 @echo off
-python src/aws_integration.py
+echo Checking for prereqs...
+
+python --version
+if errorlevel 1 goto :errorNoPython
+echo Python is installed
+
+python -c "import boto3"
+if errorlevel 1 goto :errorNoBoto3
+echo Boto3 is installed
+goto :start
+
+:errorNoPython
+echo;
+echo Python is not installed on your system.
+echo or your PATH variable is not set correctly.
+echo Now opening the download URL.
+start "" "https://www.python.org/downloads/windows/"
+timeout 20
+goto :EOF
+
+:errorNoBoto3
+pip install boto3 --user
+
+:start
+echo prereqs installed! Starting
+py src/aws_integration.py
 timeout 100
